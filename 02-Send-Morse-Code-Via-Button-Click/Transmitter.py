7e1249fb7c418d8821bef21529536fb94046f68e
Grant <grantwinney@gmail.com>
2016-05-16 07:20:05 -0500

    Continuing work on interpreting button clicks as valid morse code

--- 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
+++ 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
@@ -4,26 +4,81 @@ import RPi.GPIO as GPIO
 import datetime
 import InternationalMorseCode as ICM
 
-UNIT_TIME = .25
-ACCEPTABLE_DELTA = UNIT_TIME / 4
+BASE_TIME = 1000000  # in microseconds, 1000000 ms = 1 sec
+TOLERANCE = BASE_TIME / 4
 
-PIN = 6
+INPUT_PIN = 31
+
+segments = ""
+letters = []
+words = []
+last_edge = GPIO.LOW
+press = datetime.datetime.now()
+release = datetime.datetime.now()
 
 
 def my_callback(channel):
     #GPIO.output(21, GPIO.input(PIN))
+    global last_edge, press, release
 
-    if GPIO.input(PIN) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now()))
+    if GPIO.input(INPUT_PIN) == GPIO.HIGH and last_edge == GPIO.LOW:
+        #print('\n▼  at ' + str(datetime.datetime.now()))
+        last_edge = GPIO.HIGH
+        press = datetime.datetime.now()
+        detect_end_of_letter(press, release)
+
+    elif GPIO.input(INPUT_PIN) == GPIO.LOW and last_edge == GPIO.HIGH:
+        #print('\n ▲ at ' + str(datetime.datetime.now()))
+        last_edge = GPIO.LOW
+        release = datetime.datetime.now()
+        interpret_input(press, release)
+
+
+def interpret_input(press, release):
+    global segments
+
+    delta = calc_delta(press, release)
+    print('Button click timespan: ' + str(delta))
+
+    if (delta > (BASE_TIME - TOLERANCE)) and (delta < (BASE_TIME + TOLERANCE)):
+        print 'received dot'
+        segments += '.'
+        #print segments
+    elif (delta > ((BASE_TIME * 3) - TOLERANCE)) and (delta < ((BASE_TIME * 3) + TOLERANCE)):
+        print 'received dash'
+        segments += '-'
+        print segments
+
+
+def detect_end_of_letter(press, release):
+    global segments
+
+    delta = calc_delta(release, press)
+    if (delta > ((BASE_TIME * 3) - TOLERANCE)) and (delta < ((BASE_TIME * 3) + TOLERANCE)):
+        #print 'edge of letter detected!'
+        code = ICM.symbols.get(segments, '')
+        if code != '':
+            print 'detected letter is ' + code
+            letters.append(code)
+            segments = ""
+        else:
+            print 'invalid combo: ' + segments
+
+
+def calc_delta(t1, t2):
+    delta = t2 - t1
+    return (delta.seconds * 1000000) + delta.microseconds
 
 
 def initialize_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PIN, GPIO.IN)#, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(PIN, GPIO.BOTH, callback=my_callback)#, bouncetime=100)
-    #GPIO.setup(21,GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(36, GPIO.OUT)  # Red LED
+    GPIO.setup(32, GPIO.OUT)  # Green LED
+
+    GPIO.setup(INPUT_PIN, GPIO.IN)
+    GPIO.add_event_detect(INPUT_PIN, GPIO.BOTH, callback=my_callback)
+
 
 try:
     initialize_gpio()
