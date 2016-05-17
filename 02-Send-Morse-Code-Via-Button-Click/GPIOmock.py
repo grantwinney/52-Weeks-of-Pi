51d1d3f9913872214a441d20816085683187f9f8
Grant Winney <gwinney@grantwMac.local>
2016-05-17 13:20:18 -0400

    WIP: Refactoring code

--- 02-Send-Morse-Code-Via-Button-Click/GPIOmock.py
+++ 02-Send-Morse-Code-Via-Button-Click/GPIOmock.py
@@ -8,8 +8,8 @@ IN = '"In" Direction for Pin'
 OUT = '"Out" Direction for Pin'
 
 
-def output(pin, signal):
-    print("Sent GPIO pin {} to {}".format(pin, _convert_signal_to_text(signal)))
+def output(channel, signal):
+    print("Sent GPIO pin {} to {}".format(channel, _convert_signal_to_text(signal)))
 
 
 def setmode(numbering_style):
@@ -20,8 +20,12 @@ def setwarnings(set_warnings):
     print("Set warnings to {}".format(set_warnings))
 
 
-def setup(pin, direction):
-    print("Set {} {}".format(direction, pin))
+def setup(channel, direction):
+    print("Set {} {}".format(direction, channel))
+
+
+def input(channel):
+    print("Some input on channel {}".format(channel))
 
 
 def _convert_signal_to_text(signal):
--- 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
+++ 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
@@ -1,65 +1,55 @@
 import datetime, threading, time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+import GPIOmock as GPIO
 import InternationalMorseCode as ICM
 
 BASE_TIME_SECONDS = 1
 TOLERANCE = BASE_TIME_SECONDS / 4.0
 
-INPUT_PIN = 31
-METRONOME_PIN = 11
+
+# Initialize GPIO settings
+def initialize_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup([11, 32, 36], GPIO.OUT)  # LEDs: Blue (metronome), Green (ok), Red (error)
+    GPIO.setup(31, GPIO.IN)
+    GPIO.add_event_detect(31, GPIO.BOTH, callback=intercept_morse_code)
 
 
-### METRONOME: Setup a blue LED to blink at the "base time" interval,
-###  which will assist in timing the button clicks
-
-is_metronome_on = False
-
-
-def metronome_tick():
-    global is_metronome_on
-
+# Blink a blue LED on/off repeatedly, to assist in sending morse code
+def metronome():
     while True:
-        if is_metronome_on == False:
-            GPIO.output(METRONOME_PIN, GPIO.HIGH)
-            is_metronome_on = True
-        else:
-            GPIO.output(METRONOME_PIN, GPIO.LOW)
-            is_metronome_on = False
+        GPIO.output(11, not GPIO.input(11))
         time.sleep(BASE_TIME_SECONDS)
 
 
-def start_metronome():
-    GPIO.setup(METRONOME_PIN, GPIO.OUT)
-    timerThread = threading.Thread(target=metronome_tick)
-    timerThread.daemon = True
-    timerThread.start()
-
-
-### TRANSMITTER: Setup pin 31 to read input when the button is clicked.
-###  Interpret and store each dot/dash, letter and word.
-
-sequence = ""
-letters = []
-words = []
 last_edge = GPIO.LOW
 press = datetime.datetime.now()
 release = datetime.datetime.now()
 
 
-def my_callback(channel):
+# Intercept a rise or fall on pin 31 (button press/release)
+def intercept_morse_code(channel):
     global last_edge, press, release
 
-    if GPIO.input(INPUT_PIN) == GPIO.HIGH and last_edge == GPIO.LOW:
+    # Button pressed - ????
+    if GPIO.input(channel) == GPIO.HIGH and last_edge == GPIO.LOW:
         last_edge = GPIO.HIGH
         press = datetime.datetime.now()
-        detect_end_of_letter(press, release)
-    elif GPIO.input(INPUT_PIN) == GPIO.LOW and last_edge == GPIO.HIGH:
+        detect_end_of_letter()
+
+    # Button released - determine what the input is
+    elif GPIO.input(channel) == GPIO.LOW and last_edge == GPIO.HIGH:
         last_edge = GPIO.LOW
         release = datetime.datetime.now()
-        interpret_input(press, release)
+        interpret_input()
 
 
-def detect_end_of_letter(press, release):
+sequence = ""
+letters = []
+words = []
+
+
+def detect_end_of_letter():
     global sequence
     base_time_in_ms = BASE_TIME_SECONDS * 1000000
     tolerance_in_ms = TOLERANCE * 1000000
@@ -69,45 +59,45 @@ def detect_end_of_letter(press, release):
     if (delta > ((base_time_in_ms * 3) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 3) + tolerance_in_ms)):
         code = ICM.symbols.get(sequence, '')
         if code != '':
-            print 'detected letter is ' + code
+            print('detected letter is ' + code)
             letters.append(code)
             sequence = ""
         else:
-            print 'invalid combo: ' + sequence
+            print('invalid combo: ' + sequence)
 
     elif (delta > ((base_time_in_ms * 7) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 7) + tolerance_in_ms)):
         code = ICM.symbols.get(sequence, '')
         if code != '':
-            print 'detected letter is ' + code
+            print('detected letter is ' + code)
             letters.append(code)
             word = ''.join(letters)
             if word == "AR":
-                print 'END OF TRANSMISSION! Thanks for playing!'
+                print('END OF TRANSMISSION! Thanks for playing!')
             words.append(word)
             letters[:] = []
             sequence = ""
         else:
-            print 'invalid combo: ' + sequence
+            print('invalid combo: ' + sequence)
 
 
-def interpret_input(press, release):
+def interpret_input():
     global sequence
     base_time_in_ms = BASE_TIME_SECONDS * 1000000
     tolerance_in_ms = TOLERANCE * 1000000
 
     delta = calc_delta(press, release)
 
-    print base_time_in_ms
-    print tolerance_in_ms
+    print(base_time_in_ms)
+    print(tolerance_in_ms)
 
     if (delta > (base_time_in_ms - tolerance_in_ms)) and (delta < (base_time_in_ms + tolerance_in_ms)):
         sequence += '.'
-        print "Timespan " + str(delta) + " : " + sequence
+        print("Timespan " + str(delta) + " : " + sequence)
     elif (delta > ((base_time_in_ms * 3) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 3) + tolerance_in_ms)):
         sequence += '-'
-        print "Timespan " + str(delta) + " : " + sequence
+        print("Timespan " + str(delta) + " : " + sequence)
     else:
-        print "Timespan " + str(delta) + " : unrecognized input"
+        print("Timespan " + str(delta) + " : unrecognized input")
 
 
 def calc_delta(t1, t2):
@@ -115,19 +105,9 @@ def calc_delta(t1, t2):
     return (delta.seconds * 1000000) + delta.microseconds
 
 
-def initialize_gpio():
-    GPIO.setmode(GPIO.BOARD)
-
-    GPIO.setup(36, GPIO.OUT)             # Red LED
-    GPIO.setup(32, GPIO.OUT)             # Green LED
-
-    GPIO.setup(INPUT_PIN, GPIO.IN)
-    GPIO.add_event_detect(INPUT_PIN, GPIO.BOTH, callback=my_callback)
-
-
 try:
     initialize_gpio()
-    start_metronome()
+    threading.Thread(target=metronome, daemon=True).start()
     message = raw_input('\nPress any key to exit.\n')
 
 
