6b445e8e452fb54df55d9880f5951800accd23b1
Grant Winney <gwinney@grantwmac.home>
2016-05-18 00:02:43 -0400

    Cleaning up code

--- 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
+++ 02-Send-Morse-Code-Via-Button-Click/Transmitter.py
@@ -1,10 +1,9 @@
 import datetime, threading, time
-# import RPi.GPIO as GPIO
-import GPIOmock as GPIO
+import RPi.GPIO as GPIO
 import InternationalMorseCode as ICM
 
 BASE_TIME_SECONDS = 1
-TOLERANCE = BASE_TIME_SECONDS / 4.0
+TOLERANCE = BASE_TIME_SECONDS / 3.0
 
 
 # Initialize GPIO settings
@@ -15,11 +14,11 @@ def initialize_gpio():
     GPIO.add_event_detect(31, GPIO.BOTH, callback=intercept_morse_code)
 
 
-# Blink a blue LED on/off repeatedly, to assist in sending morse code
+# Blink a blue LED on/off (one full cycle per BASE_TIME_SECONDS)
 def metronome():
     while True:
         GPIO.output(11, not GPIO.input(11))
-        time.sleep(BASE_TIME_SECONDS)
+        time.sleep(BASE_TIME_SECONDS / 2)
 
 
 last_edge = GPIO.LOW
@@ -31,11 +30,11 @@ release = datetime.datetime.now()
 def intercept_morse_code(channel):
     global last_edge, press, release
 
-    # Button pressed - ????
+    # Button pressed - determine if start of new letter/word
     if GPIO.input(channel) == GPIO.HIGH and last_edge == GPIO.LOW:
         last_edge = GPIO.HIGH
         press = datetime.datetime.now()
-        detect_end_of_letter()
+        detect_termination()
 
     # Button released - determine what the input is
     elif GPIO.input(channel) == GPIO.LOW and last_edge == GPIO.HIGH:
@@ -49,66 +48,77 @@ letters = []
 words = []
 
 
-def detect_end_of_letter():
+# Detect whether most recent button press is start of new letter or word
+def detect_termination():
     global sequence
-    base_time_in_ms = BASE_TIME_SECONDS * 1000000
-    tolerance_in_ms = TOLERANCE * 1000000
+    delta = calc_delta_in_sec(release, press)
 
-    delta = calc_delta(release, press)
+    # Check for start of new letter (gap equal to 3 dots)
+    if (delta >= ((BASE_TIME_SECONDS * 3) - TOLERANCE)) and (delta <= ((BASE_TIME_SECONDS * 3) + TOLERANCE)):
+        process_letter()
 
-    if (delta > ((base_time_in_ms * 3) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 3) + tolerance_in_ms)):
-        code = ICM.symbols.get(sequence, '')
-        if code != '':
-            print('detected letter is ' + code)
-            letters.append(code)
-            sequence = ""
+    # Check for start of new word (gap equal to 7 dots - but assume anything > 7 dots is valid too)
+    elif delta >= ((BASE_TIME_SECONDS * 7) - TOLERANCE):
+        process_word()
+
+    # If it's not a new letter or word, and it's a gap greater than a single dot, tell the user
+    elif delta > (BASE_TIME_SECONDS + TOLERANCE):
+        print("")
+
+
+# Process letter
+def process_letter():
+    global sequence
+    character = ICM.symbols.get(sequence, '')
+
+    if character != '':
+        print("Interpreted sequence " + sequence + " as the letter: " + character)
+        letters.append(character)
+        sequence = ""
+        return True
+    else:
+        print('Invalid sequence: ' + sequence + " (deleting current sequence)")
+        sequence = ""
+        return False
+
+
+# Process word
+def process_word():
+    if process_letter():
+        word = ''.join(letters)
+        letters[:] = []
+        if word == "AR":
+            print("End of transmission. Here's your message: " + ' '.join(words))
+            print('\nClearing previous transmission. Start a new one now...\n')
+            words[:] = []
         else:
-            print('invalid combo: ' + sequence)
-
-    elif (delta > ((base_time_in_ms * 7) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 7) + tolerance_in_ms)):
-        code = ICM.symbols.get(sequence, '')
-        if code != '':
-            print('detected letter is ' + code)
-            letters.append(code)
-            word = ''.join(letters)
-            if word == "AR":
-                print('END OF TRANSMISSION! Thanks for playing!')
             words.append(word)
-            letters[:] = []
-            sequence = ""
-        else:
-            print('invalid combo: ' + sequence)
 
 
+# Interpret button click (press/release) as dot, dash or unrecognized
 def interpret_input():
     global sequence
-    base_time_in_ms = BASE_TIME_SECONDS * 1000000
-    tolerance_in_ms = TOLERANCE * 1000000
+    delta = calc_delta_in_sec(press, release)
 
-    delta = calc_delta(press, release)
-
-    print(base_time_in_ms)
-    print(tolerance_in_ms)
-
-    if (delta > (base_time_in_ms - tolerance_in_ms)) and (delta < (base_time_in_ms + tolerance_in_ms)):
+    if (delta >= (BASE_TIME_SECONDS - TOLERANCE)) and (delta <= (BASE_TIME_SECONDS + TOLERANCE)):
         sequence += '.'
-        print("Timespan " + str(delta) + " : " + sequence)
-    elif (delta > ((base_time_in_ms * 3) - tolerance_in_ms)) and (delta < ((base_time_in_ms * 3) + tolerance_in_ms)):
+        print(str(delta) + " : Added dot to sequence:  " + sequence)
+    elif (delta >= ((BASE_TIME_SECONDS * 3) - TOLERANCE)) and (delta <= ((BASE_TIME_SECONDS * 3) + TOLERANCE)):
         sequence += '-'
-        print("Timespan " + str(delta) + " : " + sequence)
+        print(str(delta) + " : Added dash to sequence: " + sequence)
     else:
-        print("Timespan " + str(delta) + " : unrecognized input")
+        print(str(delta) + " : Unrecognized input!")
 
 
-def calc_delta(t1, t2):
-    delta = t2 - t1
-    return (delta.seconds * 1000000) + delta.microseconds
+def calc_delta_in_sec(time1, time2):
+    delta = time2 - time1
+    return delta.seconds + (delta.microseconds / 1000000.0)
 
 
 try:
     initialize_gpio()
     threading.Thread(target=metronome, daemon=True).start()
-    message = raw_input('\nPress any key to exit.\n')
+    message = raw_input("\nPress any key to exit.\n")
 
 
 finally:
