09c1cf94ec655be597e8f718bc574295ee868540
Grant Winney <gwinney@grantwmac.home>
2016-05-23 10:55:58 -0400

    Testing out gmail api

--- 03-Blink-LED-When-New-Email-Arrives/NewEmailIndicator.py
+++ 03-Blink-LED-When-New-Email-Arrives/NewEmailIndicator.py
@@ -1,27 +1,73 @@
-import imaplib, serial, struct, time
+from __future__ import print_function
+import httplib2
+import os
 
-class Mail():
-    def __init__(self):
-        self.user= ' '
-        self.password= ' '
-        #self.ser = serial.Serial('/dev/tty.usbmodem621', 9600)
-        self.M = imaplib.IMAP4_SSL('imap.gmail.com', '993')
-        self.M.login(self.user, self.password)
-        
-    def checkMail(self):
-        self.M.select()
-        self.unRead = self.M.search(None, 'UnSeen')
-        return len(self.unRead[1][0].split())
-    
-    def sendData(self):
-        self.numMessages = self.checkMail()
-        #turn the string into packed binary data to send int
-        self.ser.write(struct.pack('B', self.numMessages))
-        
-email = Mail()
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
 
-# check for new mail every minute
-while 1:
-    print 'Sending'
-    email.sendData()
-    time.sleep(60)
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+      print('Labels:')
+      for label in labels:
+        print(label['name'])
+
+
+if __name__ == '__main__':
+    main()
