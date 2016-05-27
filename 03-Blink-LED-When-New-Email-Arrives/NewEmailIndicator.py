b20efb8e59d20110ef794d1cc5aee4be45e357ce
Grant <grantwinney@gmail.com>
2016-05-27 07:12:55 -0500

    Bug fix

--- 03-Blink-LED-When-New-Email-Arrives/NewEmailIndicator.py
+++ 03-Blink-LED-When-New-Email-Arrives/NewEmailIndicator.py
@@ -8,11 +8,11 @@ def initialize_gpio():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(Gmail.PIN, GPIO.OUT)
     GPIO.setup(CHECK_NOW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(CHECK_NOW_PIN, GPIO.RISING, callback=Gmail.refresh, bouncetime=1000)
+    GPIO.add_event_detect(CHECK_NOW_PIN, GPIO.RISING, callback=check_mail_now, bouncetime=1000)
 
 
-#def check_all_mail_now(_):
-#    Gmail.refresh()
+def check_mail_now(_):
+    Gmail.refresh()
 
 
 def main():
