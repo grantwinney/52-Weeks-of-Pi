7f9a321de79f04b3b9c56c46b02a34537cea737f
Grant <grantwinney@gmail.com>
2016-08-23 00:47:55 -0400

    Added a flicker.

---
+++ 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
@@ -0,0 +1,71 @@
+import RPi.GPIO as GPIO
+import threading
+import time
+import random
+
+R = 12
+G = 33
+
+pwms = []
+
+
+def initialize_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup([R,G], GPIO.OUT, initial=GPIO.HIGH)
+
+
+def red_light():
+    p = GPIO.PWM(R, 300)
+    p.start(100)
+    pwms.append(p)
+
+    while True:
+        p.ChangeDutyCycle(random.randint(80,100))
+        rand_flicker_time()
+
+
+def green_light():
+    p = GPIO.PWM(G, 300)
+    p.start(0)
+    pwms.append(p)
+
+    while True:
+        p.ChangeDutyCycle(random.randint(10,20))
+        rand_flicker_time()
+
+
+def rand_flicker_time():
+    time.sleep(random.randint(3,15) / 100.0)
+
+
+#def dying_down():
+    
+
+
+def light_a_fire():
+    threads = []
+    threads.append(threading.Thread(target=red_light))
+    threads.append(threading.Thread(target=green_light))
+    #threads.append(threading.Thread(target=dying_down))
+    for t in threads:
+        t.daemon = True
+        t.start()
+    for t in threads:
+        t.join()
+
+
+def main():
+    try:
+        initialize_gpio()
+        print("\nPress ^C (control-C) to exit the program.\n")
+        light_a_fire()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for p in pwms:
+            p.stop()
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 07-Candle-Simulation-on-RGB-LED/README.md
@@ -0,0 +1,13 @@
+# Candle Simulation on an RGB LED
+
+I've been meaning to devote some time to learning how these RGB LEDs work, so this week is just a little experimenting with it, and getting it to randomly cycle through the different color combinations.
+
+## To Run
+
+The RgbLed.py file cycles through colors. Run it using: `python3 RgbLed.py`
+
+The RgbLedPwm.py file cycles through colors too, but uses PWM (pulse-width modulation) to do so very smoothly.
+
+## Notes
+
+The pins for each color are listed at the top of each script. Change as necessary.
