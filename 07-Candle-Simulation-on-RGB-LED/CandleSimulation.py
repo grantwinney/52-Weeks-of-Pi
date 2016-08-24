a315c117e218a7036e0cdf616568490347f4014f
Grant <grantwinney@gmail.com>
2016-08-23 22:44:18 -0400

    WIP: Getting less green to show as the candle fades out.

--- 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
+++ 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GPIO
 import threading
 import time
 import random
-import math
 
 R = 37
 G = 33
@@ -14,7 +13,7 @@ aging = 1.0
 
 def initialize_gpio():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup([R,G], GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup([R,G], GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(BUTTON, GPIO.FALLING, throw_on_a_log, 250)
 
@@ -23,9 +22,12 @@ def red_light():
     p = GPIO.PWM(R, 300)
     p.start(100)
     pwms.append(p)
-
+    GPIO.output(R, GPIO.HIGH)
     while True:
-        p.ChangeDutyCycle(rand_flicker_level(80,100))
+        if aging > .1:
+            p.ChangeDutyCycle(rand_flicker_level(75,100))
+        else:
+            p.ChangeDutyCycle(rand_flicker_level(75,100) / (aging * 4))
         rand_flicker_sleep()
 
 
@@ -33,9 +35,13 @@ def green_light():
     p = GPIO.PWM(G, 300)
     p.start(0)
     pwms.append(p)
-
+    GPIO.output(G, GPIO.HIGH)
     while True:
-        p.ChangeDutyCycle(rand_flicker_level(10,20))
+        if aging > .1:
+            p.ChangeDutyCycle(rand_flicker_level(15,20))
+        else:
+            p.ChangeDutyCycle(rand_flicker_level(1,3)/10)
+        #p.ChangeDutyCycle(rand_flicker_level(15,20) if aging > .05 else 0)
         rand_flicker_sleep()
 
 
@@ -44,22 +50,24 @@ def rand_flicker_sleep():
 
 
 def rand_flicker_level(min_lvl, max_lvl):
-    math.floor(random.randint(min_lvl, max_lvl) * aging)
+    return random.randint(min_lvl, max_lvl) * aging
 
 
 def dying_down():
     global aging
-    aging -= 0.02
-    if aging < 0:
-        aging = 0
-    time.sleep(1)
+    while True:
+        if aging > .05:
+            aging -= .01
+        elif aging > .01:
+            aging -= .001
+        else:
+            aging = 0 
+        time.sleep(.25)
 
 
 def throw_on_a_log(_):
     global aging
-    aging += 0.25
-    if aging > 100:
-        aging = 100
+    aging = min(aging + 0.25, 1.0)
 
 
 def light_a_fire():
