8f9be2b9234259288b07a6647cef8982e3c66465
Grant <grantwinney@gmail.com>
2016-08-25 08:18:45 -0400

    Refactoring and cleanup.

--- 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
+++ 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
@@ -9,24 +9,22 @@ G = 33
 BUTTON = 22
 
 pwms = []
-aging = 1.0
-green_dc = 100
+intensity = 1.0
 
 
 def initialize_gpio():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup([R,G], GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(BUTTON, GPIO.FALLING, throw_on_a_log, 250)
+    GPIO.add_event_detect(BUTTON, GPIO.FALLING, fan_the_flame, 250)
 
 
 def red_light():
     p = GPIO.PWM(R, 300)
     p.start(100)
     pwms.append(p)
-    GPIO.output(R, GPIO.HIGH)
     while True:
-        p.ChangeDutyCycle(random.randint(75, 100) * (aging + ((1 - aging) / 5)) if green_dc > 0 else 0)
+        p.ChangeDutyCycle(random.randint(75, 100) * (intensity + ((1 - intensity) / 10)))
         rand_flicker_sleep()
 
 
@@ -35,34 +33,32 @@ def green_light():
     p = GPIO.PWM(G, 300)
     p.start(0)
     pwms.append(p)
-    GPIO.output(G, GPIO.HIGH)
     while True:
-        green_dc = random.randint(9, 12) * math.pow(aging, 0.5 / aging) if aging > 0.01 else 0
-        p.ChangeDutyCycle(green_dc)
+        p.ChangeDutyCycle(random.randint(9, 12) * math.pow(intensity, 1 / intensity) if intensity > 0 else 0)
         rand_flicker_sleep()
 
 
 def rand_flicker_sleep():
-    time.sleep(random.randint(3,15) / 100.0)
+    time.sleep(random.randint(3,10) / 100.0)
 
 
-def dying_down():
-    global aging
+def burning_down():
+    global intensity
     while True:
-        aging = max(aging - .01, 0)
+        intensity = max(intensity - .01, 0)
         time.sleep(.25)
 
 
-def throw_on_a_log(_):
-    global aging
-    aging = min(aging + 0.25, 1.0)
+def fan_the_flame(_):
+    global intensity
+    intensity = min(intensity + 0.25, 1.0)
 
 
-def light_a_fire():
+def light_candle():
     threads = [
         threading.Thread(target=red_light),
         threading.Thread(target=green_light),
-        threading.Thread(target=dying_down)
+        threading.Thread(target=burning_down)
     ]
     for t in threads:
         t.daemon = True
@@ -75,7 +71,7 @@ def main():
     try:
         initialize_gpio()
         print("\nPress ^C (control-C) to exit the program.\n")
-        light_a_fire()
+        light_candle()
     except KeyboardInterrupt:
         pass
     finally:
