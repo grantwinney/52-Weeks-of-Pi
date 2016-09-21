bf7f17cf352b587ec9afb72a07cc5e2bb002ef46
Grant <grantwinney@gmail.com>
2016-09-21 03:51:26 -0400

    Added joystick center x/y coords to variables since joystick doesn't rest at exact half-way point. Refactored code to do as few calculations as possible if button is pressed or joystick is at rest.

--- 08-Analog-Joystick-Color-Wheel-Test/joystick-color-wheel-test.py
+++ 08-Analog-Joystick-Color-Wheel-Test/joystick-color-wheel-test.py
@@ -13,13 +13,14 @@ mcp3008_switch_channel = 0
 mcp3008_x_voltage_channel = 1
 mcp3008_y_voltage_channel = 2
 
-# Define delay between readings (s)
-delay = 0.01
-
 # Define RGB channels
 red_led = 36
 green_led = 31
 blue_led = 37
+
+# Center positions when joystick is at rest
+center_x_pos = 530
+center_y_pos = 504
 
 # Initialize GPIO
 GPIO.setmode(GPIO.BOARD)
@@ -39,8 +40,8 @@ def read_spi_data_channel(channel):
 
 
 def convert_coordinates_to_angle(x, y):
-    dx = x - 512
-    dy = y - 512
+    dx = x - center_x_pos
+    dy = y - center_y_pos
     rads = math.atan2(-dy, dx)
     rads %= 2 * math.pi
     return math.degrees(rads)
@@ -60,40 +61,54 @@ def interpret_angle_for_led(angle, led_peak_angle):
         return 100 - ((360 - angle) * (100/120))
 
 
+def is_joystick_near_center(x, y):
+    dx = math.fabs(x - center_x_pos)
+    dy = math.fabs(y - center_y_pos)
+    return dx < 20 and dy < 20
+
+
 def main():
     pwm_r = GPIO.PWM(red_led, 300)
     pwm_g = GPIO.PWM(green_led, 300)
     pwm_b = GPIO.PWM(blue_led, 300)
 
-    for p in [pwm_r, pwm_g, pwm_b]:
-        p.start(100)
+    pwm_instances = [pwm_r, pwm_g, pwm_b]
+
+    for p in pwm_instances:
+        p.start(0)
 
     try:
         while True:
+            # If joystick switch is pressed down, turn off LEDs
+            switch = read_spi_data_channel(mcp3008_switch_channel)
+            if (switch == 0):
+                for p in pwm_instances:
+                    p.ChangeDutyCycle(0)
+                continue
+
             # Read the joystick position data
-            vrx_pos = read_spi_data_channel(mcp3008_x_voltage_channel)
-            vry_pos = read_spi_data_channel(mcp3008_y_voltage_channel)
-            angle = convert_coordinates_to_angle(vrx_pos, vry_pos)
+            x_pos = read_spi_data_channel(mcp3008_x_voltage_channel)
+            y_pos = read_spi_data_channel(mcp3008_y_voltage_channel)
 
-            pwm_r.ChangeDutyCycle(interpret_angle_for_led(angle, 90))
-            pwm_g.ChangeDutyCycle(interpret_angle_for_led(angle, 330))
-            pwm_b.ChangeDutyCycle(interpret_angle_for_led(angle, 210))
+            # If joystick is at rest, turn on all LEDs (producing 'white')
+            if is_joystick_near_center(x_pos, y_pos):
+                for p in pwm_instances:
+                    p.ChangeDutyCycle(100)
+                continue
 
-            # Read switch state
-            swt_val = read_spi_data_channel(mcp3008_switch_channel)
+            # Adjust duty cycle of LEDs based on joystick position
+            angle = convert_coordinates_to_angle(x_pos, y_pos)
+            pwm_r.ChangeDutyCycle(interpret_angle_for_led(angle, 90))   # red
+            pwm_g.ChangeDutyCycle(interpret_angle_for_led(angle, 330))  # green
+            pwm_b.ChangeDutyCycle(interpret_angle_for_led(angle, 210))  # blue
 
-            # Print out results
-            # print("--------------------------------------------")
-            # print("X : {}  Y : {}  Switch : {}".format(vrx_pos, vry_pos, swt_val))
-
-            # Wait before repeating loop
-            # time.sleep(delay)
+            print("Coordinate : ({},{})  --  Angle : {}".format(x_pos, y_pos, angle))
 
     except KeyboardInterrupt:
         pass
 
     finally:
-        for p in [pwm_r, pwm_g, pwm_b]:
+        for p in pwm_instances:
             p.stop()
         GPIO.cleanup()
 
