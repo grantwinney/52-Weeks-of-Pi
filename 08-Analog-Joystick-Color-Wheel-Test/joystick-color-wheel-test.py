d036fe3da320c110e197944c784ce7dc2967314b
Grant Winney <gwinney@grantwmac.home>
2016-09-22 01:51:52 -0400

    Added documentation to each function. Some other refactoring.

--- 08-Analog-Joystick-Color-Wheel-Test/joystick-color-wheel-test.py
+++ 08-Analog-Joystick-Color-Wheel-Test/joystick-color-wheel-test.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import math
 import RPi.GPIO as GPIO
 # import GPIOmock as GPIO
@@ -22,23 +23,25 @@ blue_led = 37
 center_x_pos = 530
 center_y_pos = 504
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup([red_led, green_led, blue_led], GPIO.OUT, initial=GPIO.LOW)
 
-
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
 def read_spi_data_channel(channel):
     """
-    :param channel: integer
-    :return:
+    Read in SPI data from the channel and return a coordinate position
+    :param channel: integer, between 0-7
+    :return: integer, between 0-1023 indicating joystick position
     """
     adc = spi.xfer2([1, (8+channel) << 4, 0])
     return ((adc[1] & 3) << 8) + adc[2]
 
 
 def convert_coordinates_to_angle(x, y):
+    """
+    Convert an x,y coordinate pair representing joystick position,
+    and convert it to an angle relative to the joystick center (resting) position
+    :param x: integer, between 0-1023 indicating position on x-axis
+    :param y: integer, between 0-1023 indicating position on y-axis
+    :return: integer, between 0-359 indicating angle in degrees
+    """
     dx = x - center_x_pos
     dy = y - center_y_pos
     rads = math.atan2(-dy, dx)
@@ -47,10 +50,22 @@ def convert_coordinates_to_angle(x, y):
 
 
 def adjust_angle_for_perspective_of_current_led(angle, led_peak_angle):
+    """
+    Take the current LED into account, and rotate the coordinate plane 360° to make PWM calculations easier
+    :param angle: integer, between 0-359 indicating current angle of joystick position
+    :param led_peak_angle: integer, between 0-359 indicating position of LED we're currently interested in
+    :return: integer, between 0-359 indicating new angle relative to the current LED under consideration
+    """
     return ((angle - led_peak_angle) + 360) % 360
 
 
-def interpret_angle_for_led(angle, led_peak_angle):
+def calculate_next_pwm_value_for_led(angle, led_peak_angle):
+    """
+    Calculate the next PWM duty cycle value for the current LED and joystick position (angle)
+    :param angle: integer, between 0-359 indicating current angle of joystick position
+    :param led_peak_angle: integer, between 0-359 indicating position of LED we're currently interested in
+    :return: integer, between 0-100 indicating the next PWM duty cycle value for the LED
+    """
     angle = adjust_angle_for_perspective_of_current_led(angle, led_peak_angle)
     if 120 < angle < 240:
         return 0
@@ -61,12 +76,26 @@ def interpret_angle_for_led(angle, led_peak_angle):
 
 
 def is_joystick_near_center(x, y):
+    """
+    Compare the current joystick position to resting position and decide if it's close enough to be considered "center"
+    :param x: integer, between 0-1023 indicating position on x-axis
+    :param y: integer, between 0-1023 indicating position on y-axis
+    :return: boolean, indicating whether or not the joystick is near the center (resting) position
+    """
     dx = math.fabs(x - center_x_pos)
     dy = math.fabs(y - center_y_pos)
     return dx < 20 and dy < 20
 
 
 def main():
+    """
+    Initializes GPIO and PWM, then sets up a loop to continually read the joystick position and calculate the next set
+    of PWM value for the RGB LED. When user hits ctrl^c, everything is cleaned up (see 'finally' block)
+    :return: None
+    """
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup([red_led, green_led, blue_led], GPIO.OUT, initial=GPIO.LOW)
+
     pwm_r = GPIO.PWM(red_led, 300)
     pwm_g = GPIO.PWM(green_led, 300)
     pwm_b = GPIO.PWM(blue_led, 300)
@@ -97,9 +126,9 @@ def main():
 
             # Adjust duty cycle of LEDs based on joystick position
             angle = convert_coordinates_to_angle(x_pos, y_pos)
-            pwm_r.ChangeDutyCycle(interpret_angle_for_led(angle, 90))   # red
-            pwm_g.ChangeDutyCycle(interpret_angle_for_led(angle, 330))  # green
-            pwm_b.ChangeDutyCycle(interpret_angle_for_led(angle, 210))  # blue
+            pwm_r.ChangeDutyCycle(calculate_next_pwm_value_for_led(angle, 90))   # red
+            pwm_g.ChangeDutyCycle(calculate_next_pwm_value_for_led(angle, 330))  # green
+            pwm_b.ChangeDutyCycle(calculate_next_pwm_value_for_led(angle, 210))  # blue
 
             # print("Position : ({},{})  --  Angle : {}".format(x_pos, y_pos, round(angle, 2)))
 
